f632e5e390252bb1eb773222f03affeac9950cc4
marfrancis <marcelo@mediasolutionproducoes.com.br>
2019-06-24 19:01:08 -0300

    arquivos finalizados

---
+++ cadastro.php
@@ -0,0 +1,23 @@
+<?php 
+
+$dsn = "mysql:host=localhost;dbname=landpage;port=3306";
+$user = "root";
+$pass = "";
+
+$conexao = new PDO($dsn, $user, $pass);
+
+
+$query = $conexao->prepare('INSERT INTO cadastro (id, nome, email)
+VALUES (null,"'.$_POST['nome'].'","'.$_POST['email'].'")');
+$resultado = $query->execute();
+
+
+
+if($resultado){
+    header('Location: sucesso.php'); ;
+} else {
+    echo "Deu errado";
+}
+
+
+
---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+
+<title>Emprestimo Pessoal</title>
+</head>
+<body class="fundo">
+
+
+<div class="container-fluid ">
+<div class="row">
+<div class="col-md-8  mx-auto slogan">
+<h1 class="texto" >Empréstimo Pessoal</h1>
+<h4 class="texto">Dinheiro na sua Conta em até 1 dia útil. Totalmente Online sem Burocracia! Solicite uma Proposta. Cadastre seu email para ter acesso</h4>
+</div>
+<div class="col-md-4 mt-5 mx-auto">
+<div class="jumbotron">
+<form action="cadastro.php" method="post" enctype="multipart/form-data">
+<div class="form-group">
+<h3>Cadastrar Agora!</h3><br>
+<label for="nome">Nome</label>
+<input type="text" name="nome" class="form-control"  id="nome" placeholder="Digite seu nome">
+</div>
+<div class="form-group">
+<label for="email">Email</label>
+<input type="email" step="any" name="email" class="form-control" id="precoProduto" placeholder="Digite seu email">
+</div>
+
+<br>
+<button class="btn btn-success" type="submit">Enviar</button>
+
+</form>
+
+</div>
+</div>
+</div>
+
+
+
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
